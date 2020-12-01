541119459774a3395a1c96a7ce6f2f9e6f10197b
Trần Minh Hiếu <46083795+tranminhhieulkqn@users.noreply.github.com>
2020-12-02 00:20:49 +0700

    * Add new API for 3 models.

--- main.py
+++ main.py
@@ -23,20 +23,27 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def get_file(request_):
+    # check if the post request has the file part
+    if 'file' not in request_.files:
+        flash('No file part')
+        return redirect(request_.url)
+    file = request_.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request_.url)
+    if file and allowed_file(file.filename):
+        return file
+    return None
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
+        file = get_file(request_=request)
+        if file is not None:
             try:
                 labels = my_model.get_lables()
                 xception = my_model.prediction(model="Xception",
@@ -66,6 +73,91 @@ def home_page():
         return render_template('index.html')
 
 
+@app.route('/densenet201', methods=['POST'])
+def result_densenet201():
+    file = get_file(request_=request)
+    if file is not None:
+        try:
+            labels = my_model.get_lables()
+            densenet201 = my_model.prediction(model="DenseNet201",
+                                              image_request=file)
+        except():
+            return jsonify({
+                "success": False,
+                "message": "File not exist!"
+            }), 404
+
+        return jsonify({
+            "success": True,
+            "message": "Predicted Results",
+            "Labels": labels.tolist(),
+            "DenseNet201": [round(num, 2) for num in densenet201[0].tolist()],
+            "DenseNet201 Predicted": densenet201[1]
+        }), 200
+    else:
+        return jsonify({
+            "success": False,
+            "message": "File not exist!"
+        }), 404
+
+
+@app.route('/inceptionv3', methods=['POST'])
+def result_inceptionv3():
+    file = get_file(request_=request)
+    if file is not None:
+        try:
+            labels = my_model.get_lables()
+            inceptionv3 = my_model.prediction(model="InceptionV3",
+                                              image_request=file)
+        except():
+            return jsonify({
+                "success": False,
+                "message": "File not exist!"
+            }), 404
+
+        return jsonify({
+            "success": True,
+            "message": "Predicted Results",
+            "Labels": labels.tolist(),
+            "InceptionV3": [round(num, 2) for num in inceptionv3[0].tolist()],
+            "InceptionV3 Predicted": inceptionv3[1]
+        }), 200
+    else:
+        return jsonify({
+            "success": False,
+            "message": "File not exist!"
+        }), 404
+
+
+@app.route('/xception', methods=['POST'])
+def result_xception():
+    file = get_file(request_=request)
+    if file is not None:
+        try:
+            labels = my_model.get_lables()
+            xception = my_model.prediction(model="Xception",
+                                           image_request=file)
+        except():
+            return jsonify({
+                "success": False,
+                "message": "File not exist!"
+            }), 404
+
+        return jsonify({
+            "success": True,
+            "message": "Predicted Results",
+            "Labels": labels.tolist(),
+            "Xception": [round(num, 2) for num in xception[0].tolist()],
+            "Xception Predicted": xception[1]
+        }), 200
+    else:
+        return jsonify({
+            "success": False,
+            "message": "File not exist!"
+        }), 404
+
+
 if __name__ == '__main__':
+    app.run()
     if my_model is None:
         my_model = ModelGeneral()
